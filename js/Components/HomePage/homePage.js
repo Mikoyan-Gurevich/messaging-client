71f4ab1d5871587fc404b11eb6b2ea716eb1ca63
Gaurav Sharma <gaurav.min.js@gmail.com>
2017-10-07 23:41:54 +0530

    designed login screen

--- js/Components/HomePage/homePage.js
+++ js/Components/HomePage/homePage.js
@@ -11,7 +11,24 @@ class HomePage extends React.Component {
 
     render() {
         return (
-            <div><Link to='/signup'>Sign Up Now !</Link></div>
+            <div className='homePage'>
+                <div className='centerPart'>
+                    <div className='innerCenter'>
+                        <div className='title'>Welcome to Messaging</div>
+                        <div>
+                            <input placeholder='Username/ Mobile Number/ Email ID'/>
+                        </div>
+                        <div>
+                            <input  type='password' placeholder='Password'/>
+                        </div>
+                        <div><button>Login to Messaging</button></div>
+                        <div>
+                            <Link to='/signup'>Forgot Password ? </Link>
+                            <Link to='/signup'>Sign Up as a new user</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
