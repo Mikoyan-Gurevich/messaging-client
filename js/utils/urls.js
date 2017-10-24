c9b8ca4e2f5fff0a36c700db9131ac0b654bd539
Gaurav Sharma <gaurav.min.js@gmail.com>
2017-10-24 21:13:44 +0530

    Changed url domainname

--- js/utils/urls.js
+++ js/utils/urls.js
@@ -1,4 +1,4 @@
 // const domainURL = 'https://messaging-server.gsharma.com';
-const domainURL = window.location.hostname;
+const domainURL = window.location.hostname + ':3001';
 export const signup = domainURL + '/signup';
 export const login = domainURL + '/login';
