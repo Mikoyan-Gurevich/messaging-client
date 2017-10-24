9359e1f8bd08353117b4fbbbb1a9126a66a2e82d
Gaurav Sharma <gaurav.min.js@gmail.com>
2017-10-24 21:04:26 +0530

    Added / to url so that request to server goes to referrer only

--- js/utils/urls.js
+++ js/utils/urls.js
@@ -1,4 +1,4 @@
 // const domainURL = 'https://messaging-server.gsharma.com';
-const domainURL = 'http://localhost:3001';
+const domainURL = '';
 export const signup = domainURL + '/signup';
 export const login = domainURL + '/login';
