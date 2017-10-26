2234c120229ff302510dacd0a86a5ea7a4fb9da9
Gaurav Sharma <gaurav.min.js@gmail.com>
2017-10-27 01:08:12 +0530

    Added favicon

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|gif)$/,
-                use: ['file-loader']
+                use: ['file-loader?name=[name].[ext]']
             }
         ]
     },
