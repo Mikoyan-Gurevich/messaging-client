0db09c1b3566b1a8b65311b9494a571c61d4e962
Gaurav Sharma <gaurav.min.js@gmail.com>
2017-12-02 23:50:03 +0530

    Adding gallery to app

---
+++ js/Components/Gallery/gallery.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './gallery.scss';
+
+class Gallery extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+    render() {
+        return <div></div>
+    }
+}
