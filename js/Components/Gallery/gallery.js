b41cc489b6c32a1fe86ce98005ed324fcefad850
Gaurav Sharma <gaurav.min.js@gmail.com>
2017-12-03 21:32:46 +0530

    gallery

--- js/Components/Gallery/gallery.js
+++ js/Components/Gallery/gallery.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
 import './gallery.scss';
 
+const ParentContainer = styled.div`
+width: 80%;
+margin: auto;
+height: 200px;
+background-color: red;
+`
 class Gallery extends React.Component {
     constructor() {
         super();
         this.state = {};
     }
     render() {
-        return <div></div>
+        return <div>
+            <ParentContainer />
+        </div>
     }
 }
+
+export default Gallery;
--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './main.scss';
 import Homepage from './Components/HomePage/homePage';
+import Gallery from './Components/Gallery/gallery';
 import * as OfflinePluginRuntime from 'offline-plugin/runtime';
 import english from '../translations/english.json';
 import hindi from '../translations/hindi.json';
-import db from '../js/db/db';
+//import db from '../js/db/db';
 
 OfflinePluginRuntime.install();
 
@@ -15,4 +16,4 @@ window.getStringInSelectedlanguage = code => languageObj[window.localStorage && 
 
 document.title = getStringInSelectedlanguage('appTitle');
 
-ReactDOM.render(<Homepage />, document.getElementById('app'));
+ReactDOM.render(<Gallery />, document.getElementById('app'));
